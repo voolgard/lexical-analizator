13d5758a793897703bf72d90feb22d0e49044a9c
voolgard <voolgard@yahoo.com>
2022-11-06 20:06:21 +1000

    fix cli error

--- lexical-analizator-cli.py
+++ lexical-analizator-cli.py
@@ -1,10 +1,8 @@
 import sys
 from lexicalanalizator.LexicalAnalizator import getLexemes
 
-line = "[[[]]]\n[][]"
-print(getLexemes(line.encode('utf-8')))
-# if len(sys.argv) > 1:
-#     print(getLexemes(sys.argv[1].encode('utf-8')))
-# else:
-#     print('Argument not found.')
-#     print('Use: python3 lexical-analizator-cli.py "line"')
+if len(sys.argv) > 1:
+    print(getLexemes(sys.argv[1].encode('utf-8')))
+else:
+    print('Argument not found.')
+    print('Use: python3 lexical-analizator-cli.py "line"')
